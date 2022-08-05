076e030fda9d4902bfd4b6fd864c8c6fc6de78c2
ebrahemsamer2 <ebrahemsamer2@gmail.com>
2022-08-05 03:54:11 +0300

    Implementing Task Model and Relations

--- app/Models/Todolist.php
+++ app/Models/Todolist.php
@@ -10,8 +10,14 @@ class Todolist extends Model
     use HasFactory;
     protected $fillable = ['title', 'description', 'user_id'];
 
+    // Relations
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
     }
 }
